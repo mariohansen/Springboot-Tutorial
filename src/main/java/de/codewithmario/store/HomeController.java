5dd6128b4766ccdd2147405f085cc85ec35b943a
mariohansen <mario.hansen@haw-hamburg.de>
2025-04-01 12:21:59 +0200

    spring boot tutorial

---
+++ src/main/java/de/codewithmario/store/HomeController.java
@@ -0,0 +1,18 @@
+package de.codewithmario.store;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @Value("${spring.application.name}")
+    private String appName;
+
+    @RequestMapping("/")
+    public String index(){
+        System.out.println("appname: " + appName);
+        return "index.html";
+    }
+
+}
